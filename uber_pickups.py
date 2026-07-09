94e0be6caca70b6c16ff3b15e877533e9af8f1db
Dobó H. Lilla <helena.dobo@stud.uni-corvinus.hu>
2026-07-09 17:10:51 +0200

    dinamikus csúszka a térképhez

--- uber_pickups.py
+++ uber_pickups.py
@@ -27,5 +27,12 @@ st.subheader('Number of pickups by hours')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24,range=(0,24))[0]
 st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups')
-st.map(data)
+# st.subheader('Map of all pickups')
+# st.map(data)
+
+hour_to_filter = st.slider('hour',0,23,17) # min: 0h, max: 23h, default:17h)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Maps of all pickoups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+
